ff9ba020467b64ca19f7bb6f687a07ae0dc00e53
Sourabh Bhat <heySourabh@gmail.com>
2018-05-20 22:17:40 +0530

    Failing test

--- test/main/MainTest.java
+++ test/main/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
     @Test
     public void addPolynomials() {
         double[] p1 = {2, 5, 6};
-        double[] p2 = {1, 7, 19};
+        double[] p2 = {1, 7, 16};
 
         double[] expected = new double[]{3, 12, 25};
         double[] actual = Main.addPolynomials(p1, p2);
