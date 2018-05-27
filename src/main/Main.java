865797bb4525c23ff5403e18927ca45e0f940c5e
Sourabh Bhat <heySourabh@gmail.com>
2018-05-27 22:39:34 +0530

    modified

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,20 @@
 package main;
 
+import java.util.*;
 public class Main {
+    public static void main(String... args){
+        addPolynomials(new double[]{1,2,3}, new double[]{1,4,5});
+    }
+
     static double[] addPolynomials(double[] coeff1, double[] coeffs2) {
         double[] res = new double[coeff1.length];
         for (int i = 0; i < coeff1.length; i++) {
             res[i] = coeff1[i] + coeffs2[i];
         }
+        // Little java 9
+        List<String> strs = List.of("string1", "string2");
+        System.out.println(strs);
+        
         return res;
     }
 }
